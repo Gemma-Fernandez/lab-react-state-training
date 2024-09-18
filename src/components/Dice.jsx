d5caa26ed4addb1d90f0b0930e3da593df670d75
Gemma Fernandez <gemmazuera@gmail.com>
2024-09-18 19:39:01 +0200

    iteracion 4 acabada

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import imgVacia from "../assets/images/dice-empty.png";
+import img1 from "../assets/images/dice1.png";
+import img2 from "../assets/images/dice2.png";
+import img3 from "../assets/images/dice3.png";
+import img4 from "../assets/images/dice4.png";
+import img5 from "../assets/images/dice5.png";
+import img6 from "../assets/images/dice6.png";
+
 import { useState } from "react";
 
 
 function Dice() {
 
-const[imgCubo, setImgCubo] = useState(imgVacia)
+  const totalImages=[img1, img2, img3, img4, img5, img6];
+
+  const[imgCubo, setImgCubo] = useState(imgVacia)
+
 const handleImgCubo= ()=>{
-    const randomImg= Math.floor(Math.random())
-    setImgCubo()
+    const randomImg= Math.floor(Math.random()* totalImages.length)
+    setImgCubo(totalImages[randomImg])
 
 }
 
@@ -16,7 +26,7 @@ const handleImgCubo= ()=>{
     <div>
         <img 
         src={imgCubo} 
-        alt="imagen vacia" 
+        alt="imagen dado vacia" 
         onClick={handleImgCubo}
         className="cubo"/>
     </div>
